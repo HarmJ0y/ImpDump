8a246853186bfce4602f7710873645ea23666df0
jlang7 <jason_lang@optum.com>
2016-11-29 11:39:07 -0600

    Update setup.sh
    The Impacket repo is now on GitHub.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 echo 'Downloading Impacket...'
 
-svn checkout http://impacket.googlecode.com/svn/trunk/ /tmp/impacket-svn/
+git clone https://github.com/CoreSecurity/impacket.git /tmp/impacket-git/
 
 echo 'Installing Impacket...'
-cd /tmp/impacket-svn/
+cd /tmp/impacket-git/
 python setup.py install
 cd -
 
